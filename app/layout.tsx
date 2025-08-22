a89fe59a7135a11fc5dccf6c3254f5009c1422c2
sakshiprajapati-ui <prajapatisakshi026@gmail.com>
2025-08-22 19:27:12 +0530

    installed navigation menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/app/components/Header/Header";
+import Footer from "@/app/components/Footer/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +27,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={` ${geistSans.variable} ${geistMono.variable} antiliased`}
+      ><Header/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
