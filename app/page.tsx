5284a95e03bcfff71d534e896aabd29399891f95
Himanshu Kashyap <hkashyap.developer@gmail.com>
2025-08-22 20:49:48 +0530

    Page.tsx bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,2 +1,10 @@
-import Home from "@/app/components/Home/Home"
-   <Home/>
+import React from "react";
+import Herobanner from "@/components/custom/herobanner/herobanner";
+
+export default function HomePage() {
+  return (
+    <main className="flex flex-col items-center justify-start bg-gray-50 p-6">
+      <Herobanner />
+    </main>
+  );
+}
