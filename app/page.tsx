af71d5aa4b66c9288a68a76c61250c2ab7b9b70a
sakshiprajapati-ui <prajapatisakshi026@gmail.com>
2025-08-22 21:05:07 +0530

    Added testimonial .
    Added Testimonal component for reviews.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Herobanner from "@/components/custom/herobanner/herobanner";
+import Testimonials from "@/components/custom/Testinomials/Testimonials";
 
 export default function HomePage() {
   return (
     <main className="flex flex-col items-center justify-start bg-gray-50 p-6">
       <Herobanner />
+      <Testimonials/>
     </main>
   );
 }
