f0ab1fb71946ab2e6393f28a553a3240aefd30f0
Himanshu Kashyap <hkashyap.developer@gmail.com>
2025-08-25 06:55:51 +0530

    stable version

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,14 @@ import Servicecards from "@/components/custom/cards/acard";
 import Cardsgrid from "@/components/custom/Cardsgrid/Cardsgrid";
 import Marqueetext from "@/components/custom/Marqueetext/Marqueetext";
 import Prefooter from "@/components/custom/Prefooter/Prefooter";
+import Servicegrid from "@/components/custom/Servicegrid/Servicegrid";
 
 export default function HomePage() {
   return (
     <>
       <Marqueetext />
       <Herobanner />
+      <Servicegrid />
 
       <main className="">
         <Cardsgrid />
