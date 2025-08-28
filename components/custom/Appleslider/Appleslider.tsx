afa3c7e4fda82a199e8e32b73c9fe0113a4e0561
Himanshu Kashyap <hkashyap.developer@gmail.com>
2025-08-28 14:35:59 +0530

    stable version

--- components/custom/Appleslider/Appleslider.tsx
+++ components/custom/Appleslider/Appleslider.tsx
@@ -49,40 +49,40 @@ const DummyContent = () => {
 
 const data = [
   {
-    category: "Artificial Intelligence",
-    title: "You can do more with AI.",
+    category: "Get a stunning website",
+    title: "Website Development",
     src: "/images/herobg.jpg",
     content: <DummyContent />,
   },
   {
     category: "Productivity",
-    title: "Enhance your productivity.",
-    src: "https://images.unsplash.com/photo-1531554694128-c4c6665f59c2?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "UI/UX & Graphics",
+    src: "/images/herobg.jpg",
     content: <DummyContent />,
   },
   {
     category: "Product",
-    title: "Launching the new Apple Vision Pro.",
-    src: "https://images.unsplash.com/photo-1713869791518-a770879e60dc?q=80&w=2333&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Graphic Designing",
+    src: "/images/herobg.jpg",
     content: <DummyContent />,
   },
 
   {
     category: "Product",
-    title: "Maps for your iPhone 15 Pro Max.",
-    src: "https://images.unsplash.com/photo-1599202860130-f600f4948364?q=80&w=2515&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Digital Marketing",
+    src: "/images/herobg.jpg",
     content: <DummyContent />,
   },
   {
     category: "iOS",
-    title: "Photography just got better.",
-    src: "https://images.unsplash.com/photo-1602081957921-9137a5d6eaee?q=80&w=2793&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "Building MPVs",
+    src: "/images/herobg.jpg",
     content: <DummyContent />,
   },
   {
     category: "Hiring",
-    title: "Hiring for a Staff Software Engineer",
-    src: "https://images.unsplash.com/photo-1511984804822-e16ba72f5848?q=80&w=2048&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "PR Management",
+    src: "/images/herobg.jpg",
     content: <DummyContent />,
   },
 ];
