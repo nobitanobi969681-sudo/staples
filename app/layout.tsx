05de3a280547cac7ceed24d0b52245156b9cfc11
Himanshu Kashyap <hkashyap.developer@gmail.com>
2025-08-31 03:19:39 +0530

    stable version

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antiliased bg-[url('/images/fullbg.jpg')] bg-cover bg-center max-w-[1920px] mx-auto border-x`}
+        className={`${geistSans.variable} ${geistMono.variable} antiliased bg-[url('/images/fullbg.jpg')] bg-cover bg-center `}
       >
         <Header />
         {children}
