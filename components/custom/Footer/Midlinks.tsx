b3cf8147e49847a9b8b487d4bef8ae02a10b9d19
Himanshu Kashyap <hkashyap.developer@gmail.com>
2025-08-30 18:09:18 +0530

    Phone number udpated in footer

--- components/custom/Footer/Midlinks.tsx
+++ components/custom/Footer/Midlinks.tsx
@@ -8,7 +8,7 @@ const Midlinks = () => {
       <div className="space-y-4">
         <ShimmerButton className="shadow-2xl min-w-[220px] mx-auto sm:mx-0">
           <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
-            +91 8699 81 2196
+            +91 9815 95 1565
           </span>
         </ShimmerButton>
         <ShimmerButton className="shadow-2xl min-w-[220px] mx-auto sm:mx-0">
