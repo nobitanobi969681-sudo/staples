b63ff7bb6c0832b66807c0833a51c486f0015d7e
Himanshu Kashyap <hkashyap.developer@gmail.com>
2025-08-30 19:03:33 +0530

    Home Hero Banner fully fixed UI

--- components/custom/Prefooter/Prefooter.tsx
+++ components/custom/Prefooter/Prefooter.tsx
@@ -8,11 +8,9 @@ export default function CoverDemo({ align }: any) {
       <h1 className="max-w-7xl mx-auto relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
         <SparklesText
           sparklesCount={0}
-          className="text-center sm:text-left font-semibold text-black dark:text-white text-[34px] sm:text-5xl 2xl:text-6xl px-2 sm:px-0"
+          className="text-center xl:text-left font-medium text-black dark:text-white text-[34px] sm:text-5xl 2xl:text-6xl px-2 sm:px-0"
         >
-          <span className="leading-relaxed">
-            {align} Building amazing brands at
-          </span>{" "}
+          <span className="leading-relaxed">{align} Building brands at</span>{" "}
           <Cover>warp speed </Cover>
         </SparklesText>
       </h1>
