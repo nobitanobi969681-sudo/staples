8c275d6096e267e44f188a1ad842c5c617e11a17
Himanshu Kashyap <hkashyap.developer@gmail.com>
2025-09-01 03:17:51 +0530

    stable

--- components/custom/Footer/Footer.tsx
+++ components/custom/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import Toptext from "./Toptext";
 export default function SiteFooter() {
   return (
     <footer className="bg-[url('/images/herobg.jpg')] bg-top bg-cover bt-1 bg-background/90 backdrop-blur supports-[backdrop-filter]:bg-background/99">
-      <div className="bg-[rgba(255,255,255,0.92)] dark:bg-[rgba(0,0,0,0.8)]">
+      <div className="bg-[rgba(255,255,255,0.96)] dark:bg-[rgba(0,0,0,0.92)]">
         <div className=" mx-auto px-8 py-20">
           <Toptext />
           <Midlinks />
