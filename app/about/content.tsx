4e3d4dbd863ca1037adaf75655670d312e883217
Himanshu Kashyap <hkashyap.developer@gmail.com>
2025-08-31 20:48:00 +0530

    stable

--- app/about/content.tsx
+++ app/about/content.tsx
@@ -6,18 +6,14 @@ import { TracingBeam } from "@/components/ui/tracing-beam";
 
 export default function TracingBeamDemo() {
   return (
-    <div className="my-16 mb-20 mx-auto max-w-2xl px-8">
+    <div className="my-16 mb-20 mx-auto max-w-2xl px-0 md:px-8">
       <TracingBeam className="px-6">
         <div className="max-w-2xl mx-auto antialiased pt-4 relative pl-5 sm:pl-0">
           {dummyContent.map((item, index) => (
             <div key={`content-${index}`} className="mb-10">
-              <h2 className="bg-black text-white rounded-full text-sm w-fit px-4 py-1 mb-4">
-                {item.badge}
-              </h2>
+              <p className="mb-4">{item.title}</p>
 
-              <p className="">{item.title}</p>
-
-              <div className="text-sm  prose prose-sm dark:prose-invert">
+              <div className="text-base  prose prose-sm dark:prose-invert">
                 {item?.image && (
                   <img
                     src={item.image}
@@ -91,25 +87,5 @@ const dummyContent = [
       </>
     ),
     badge: "Changelog",
-    image:
-      "https://images.unsplash.com/photo-1519681393784-d120267933ba?auto=format&fit=crop&q=80&w=3540&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    title: "Lorem Ipsum Dolor Sit Amet",
-    description: (
-      <>
-        <p>
-          Ex irure dolore veniam ex velit non aute nisi labore ipsum occaecat
-          deserunt cupidatat aute. Enim cillum dolor et nulla sunt exercitation
-          non voluptate qui aliquip esse tempor. Ullamco ut sunt consectetur
-          sint qui qui do do qui do. Labore laborum culpa magna reprehenderit ea
-          velit id esse adipisicing deserunt amet dolore. Ipsum occaecat veniam
-          commodo proident aliqua id ad deserunt dolor aliquip duis veniam sunt.
-        </p>
-      </>
-    ),
-    badge: "Launch Week",
-    image:
-      "https://images.unsplash.com/photo-1469474968028-56623f02e42e?auto=format&fit=crop&q=80&w=3506&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
--- app/about/herobanner.tsx
+++ app/about/herobanner.tsx
@@ -10,14 +10,14 @@ import { Separator } from "@/components/ui/separator";
 import { Github, Linkedin, Mail } from "lucide-react";
 export default function AboutPage() {
   return (
-    <AuroraBackground className="border-b relative min-h-[400px] flex flex-col items-center justify-center px-6 ">
+    <AuroraBackground className="border-b relative min-h-[320px] sm:min-h-[400px] flex flex-col items-center justify-center px-6 ">
       {/* Hero Section */}
       <div className="relative w-full max-w-4xl text-center mx-auto">
         <motion.h1
           initial={{ opacity: 0, y: -30 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8 }}
-          className="px-8 sm:px-0 text-4xl md:text-6xl font-bold tracking-tight relative z-10"
+          className="px-4 sm:px-0 text-4xl md:text-6xl font-bold tracking-tight relative z-10"
         >
           Crafting Digital Excellence
         </motion.h1>
@@ -26,7 +26,7 @@ export default function AboutPage() {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 0.3, duration: 0.8 }}
-          className="mt-6 text-lg md:text-xl text-muted-foreground relative z-10 max-w-xl mx-auto"
+          className="px-8 sm:px-0 mt-6 text-lg md:text-xl text-muted-foreground relative z-10 max-w-xl mx-auto"
         >
           We build next-gen digital experiences with performance, aesthetics,
           and cutting-edge technology.
