a19b7ed03a013ce1ba87b2f412fa98e4126659b6
Himanshu Kashyap <hkashyap.developer@gmail.com>
2025-09-01 15:25:08 +0530

    Stable

--- components/custom/Newquote/Newquote.tsx
+++ components/custom/Newquote/Newquote.tsx
@@ -1,81 +1,93 @@
-"use client";
-import React from "react";
-import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { cn } from "@/lib/utils";
-import {
-  IconBrandGithub,
-  IconBrandGoogle,
-  IconBrandOnlyfans,
-} from "@tabler/icons-react";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Label } from "@/components/ui/label";
+import { ShineBorder } from "@/components/magicui/shine-border";
+import { ShimmerButton } from "@/components/magicui/shimmer-button";
 
-export default function SignupFormDemo() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("Form submitted");
-  };
+export default function ShineBorderDemo() {
   return (
-    <div className="px-4 sm:px-0 ">
-      <div className="bg-[url('/images/herobg.jpg')] bg-cover mt-16 mb-16 shadow-input mx-auto w-full max-w-md rounded-0   ">
-        <div className="bg-[rgba(255,255,255,0.98)] dark:bg-[rgba(0,0,0,0.4)] p-4 md:p-8 px-8 rounded-0 ">
-          <form className="my-8 " onSubmit={handleSubmit}>
-            <div className="mb-4 flex flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-2">
-              <LabelInputContainer>
-                <Label htmlFor="firstname">First name</Label>
-                <Input id="firstname" placeholder="Tyler" type="text" />
-              </LabelInputContainer>
-              <LabelInputContainer>
-                <Label htmlFor="lastname">Last name</Label>
-                <Input id="lastname" placeholder="Durden" type="text" />
-              </LabelInputContainer>
-            </div>
-            <LabelInputContainer className="mb-4">
-              <Label htmlFor="email">Email ID</Label>
-              <Input
-                id="email"
-                placeholder="projectmayhem@fc.com"
-                type="email"
-              />
-            </LabelInputContainer>
-            <LabelInputContainer className="mb-4">
-              <Label htmlFor="textarea">Describe the project</Label>
-              <Textarea id="textarea" className="" />
-            </LabelInputContainer>
+    <div className="mx-4">
+      <Card className="relative overflow-hidden max-w-[480px] w-full mx-auto mb-16 sm:mb-32 py-8">
+        <ShineBorder shineColor={["#A07CFE", "#FE8FB5", "#FFBE7B"]} />
+        <CardHeader>
+          <CardTitle>Start Here</CardTitle>
+          <CardDescription>
+            Tell us your plan and know the cost to build it.
+          </CardDescription>
+        </CardHeader>
 
-            <button
-              className="mt-8 group/btn relative block h-10 w-full rounded-md bg-gradient-to-br from-black to-neutral-600 font-medium text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-900 dark:shadow-[0px_1px_0px_0px_#27272a_inset,0px_-1px_0px_0px_#27272a_inset]"
-              type="submit"
-            >
-              Send Message &rarr;
-              <BottomGradient />
-            </button>
+        <CardContent>
+          <form>
+            <div className="grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="name">Name</Label>
+                <Input id="name" type="text" />
+              </div>
+
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" type="email" placeholder="name@example.com" />
+              </div>
+
+              <div className="grid gap-2">
+                <Label htmlFor="phone">Phone</Label>
+                <Input id="phone" type="number" />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="phone">Service</Label>
+                <RadioGroup defaultValue="test1">
+                  <div className="flex items-center gap-3">
+                    <RadioGroupItem value="test1" id="r1" />
+                    <Label htmlFor="r1">Default</Label>
+                  </div>
+                  <div className="flex items-center gap-3">
+                    <RadioGroupItem value="test2" id="r2" />
+                    <Label htmlFor="r2">Comfortable</Label>
+                  </div>
+                  <div className="flex items-center gap-3">
+                    <RadioGroupItem value="test3" id="r3" />
+                    <Label htmlFor="r3">Compact</Label>
+                  </div>
+
+                  <div className="flex items-center gap-3">
+                    <RadioGroupItem value="test4" id="r4" />
+                    <Label htmlFor="r4">Default</Label>
+                  </div>
+                  <div className="flex items-center gap-3">
+                    <RadioGroupItem value="test5" id="r5" />
+                    <Label htmlFor="r5">Comfortable</Label>
+                  </div>
+                  <div className="flex items-center gap-3">
+                    <RadioGroupItem value="test6" id="r6" />
+                    <Label htmlFor="r6">Compact</Label>
+                  </div>
+                </RadioGroup>
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="phone">Message</Label>
+                <Textarea id="phone" />
+              </div>
+            </div>
           </form>
-        </div>
-      </div>
+        </CardContent>
+        <CardFooter>
+          <ShimmerButton className="shadow-2xl">
+            <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-relaxed text-white dark:from-white dark:to-slate-900/10 lg:text-md">
+              Request Quote
+            </span>
+          </ShimmerButton>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
-
-const BottomGradient = () => {
-  return (
-    <>
-      <span className="absolute inset-x-0 -bottom-px block h-px w-full bg-gradient-to-r from-transparent via-cyan-500 to-transparent opacity-0 transition duration-500 group-hover/btn:opacity-100" />
-      <span className="absolute inset-x-10 -bottom-px mx-auto block h-px w-1/2 bg-gradient-to-r from-transparent via-indigo-500 to-transparent opacity-0 blur-sm transition duration-500 group-hover/btn:opacity-100" />
-    </>
-  );
-};
-
-const LabelInputContainer = ({
-  children,
-  className,
-}: {
-  children: React.ReactNode;
-  className?: string;
-}) => {
-  return (
-    <div className={cn("flex w-full flex-col space-y-2", className)}>
-      {children}
-    </div>
-  );
-};
