a86129f507eef704c369a0f2f55c34c551c5c609
Himanshu Kashyap <hkashyap.developer@gmail.com>
2025-09-01 14:29:01 +0530

    stable

--- app/new-quote/page.tsx
+++ app/new-quote/page.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Herobanner from "./herobanner";
 import Features from "./features";
+import Form from "./form";
 
 const page = () => {
   return (
     <div className="min-h-screen">
       <Herobanner />
       <Features />
-      page
+      <Form />
     </div>
   );
 };
