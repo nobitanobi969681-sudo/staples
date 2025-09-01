7a462fd382ad98044345cb4d88277bac06bd17f4
Himanshu Kashyap <hkashyap.developer@gmail.com>
2025-09-01 14:29:42 +0530

    Stable

--- app/new-quote/form.tsx
+++ app/new-quote/form.tsx
@@ -13,32 +13,34 @@ import { ShineBorder } from "@/components/magicui/shine-border";
 
 export default function ShineBorderDemo() {
   return (
-    <Card className="relative overflow-hidden max-w-[480px] w-full mx-auto mb-16 py-8">
-      <ShineBorder shineColor={["#A07CFE", "#FE8FB5", "#FFBE7B"]} />
-      <CardHeader>
-        <CardTitle>Login</CardTitle>
-        <CardDescription>
-          Enter your credentials to access your account
-        </CardDescription>
-      </CardHeader>
+    <div className="mx-4">
+      <Card className="relative overflow-hidden max-w-[480px] w-full mx-auto mb-16 py-8">
+        <ShineBorder shineColor={["#A07CFE", "#FE8FB5", "#FFBE7B"]} />
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>
+            Enter your credentials to access your account
+          </CardDescription>
+        </CardHeader>
 
-      <CardContent>
-        <form>
-          <div className="grid gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input id="email" type="email" placeholder="name@example.com" />
+        <CardContent>
+          <form>
+            <div className="grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" type="email" placeholder="name@example.com" />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" type="password" />
+              </div>
             </div>
-            <div className="grid gap-2">
-              <Label htmlFor="password">Password</Label>
-              <Input id="password" type="password" />
-            </div>
-          </div>
-        </form>
-      </CardContent>
-      <CardFooter>
-        <Button className="max-w-min mx-auto">REQUEST A QUOTE</Button>
-      </CardFooter>
-    </Card>
+          </form>
+        </CardContent>
+        <CardFooter>
+          <Button className="max-w-min mx-auto">REQUEST A QUOTE</Button>
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
