ada8bdfbf5ccda2b0db10e81bbb30d41ef17b563
Himanshu Kashyap <hkashyap.developer@gmail.com>
2025-09-01 13:41:40 +0530

    Stable

--- app/new-quote/features.tsx
+++ app/new-quote/features.tsx
@@ -11,7 +11,7 @@ export default function FeaturesSectionDemo() {
             className="relative bg-gradient-to-b dark:from-neutral-900 from-neutral-100 dark:to-neutral-950 to-white p-6 rounded-3xl overflow-hidden"
           >
             <Grid size={20} />
-            <p className="text-base font-bold text-neutral-800 dark:text-white relative z-20">
+            <p className="text-base text-center font-medium text-neutral-800 dark:text-white relative z-20">
               {feature.title}
             </p>
             <p className="text-neutral-600 dark:text-neutral-400 mt-4 text-base font-normal relative z-20">
