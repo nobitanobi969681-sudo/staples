b5dd444e2146cbb4e8df1f996cf740bf19d12dde
Himanshu Kashyap <hkashyap.developer@gmail.com>
2025-09-01 21:46:03 +0530

    Stable

--- app/services/website-and-app-development/herobanner.tsx
+++ app/services/website-and-app-development/herobanner.tsx
@@ -9,13 +9,13 @@ export default function UniqueHeroWithSidebar() {
       <div className="bg-[rgba(255,255,255,0.92)] dark:bg-[rgba(0,0,0,1)] w-full flex pt-0 sm:py-16">
         {/* Hero Section */}
         <main className="flex-1 flex items-center justify-center px-0 sm:px-6 border-none">
-          <div className=" grid grid-cols-1 xl:grid-cols-2 gap-10 items-center">
+          <div className=" grid grid-cols-1 xl:grid-cols-2 gap-16 items-center">
             {/* Text Content */}
 
             <div className="flex justify-center xl:justify-end pl-0 xl:pl-20 order-2 sm:order-1">
               <div className="max-w-[800px] space-y-6 text-center xl:text-left">
                 <Prefooter />
-                <p className="mx-auto xl:mx-0 max-w-[560px] text-xl text-muted-foreground mb-8 px-2">
+                <p className="mx-auto xl:mx-0 max-w-[560px] text-xl text-muted-foreground mb-16 sm:mb-0 px-2">
                   We craft scalable, responsive, and user-friendly platforms to
                   grow your digital presence.
                 </p>
