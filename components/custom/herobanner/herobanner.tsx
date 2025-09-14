62f90beb477bb222d77d5b3696454124941d767f
Himanshu Kashyap <hkashyap.developer@gmail.com>
2025-09-14 21:38:51 +0530

    Stable

--- components/custom/herobanner/herobanner.tsx
+++ components/custom/herobanner/herobanner.tsx
@@ -22,11 +22,11 @@ export default function UniqueHeroWithSidebar() {
                   animate={{ opacity: 1, y: 0 }}
                   transition={{ duration: 0.8 }}
                 >
-                  <h1 className="max-w-2xl mx-auto relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
+                  <div className="max-w-2xl mx-auto relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
                     <span className="text-center xl:text-left font-bold leading-tight text-white text-[34px] sm:text-6xl 2xl:text-7xl px-2 sm:px-0">
                       Digital Experiences That Inspire
                     </span>
-                  </h1>
+                  </div>
                 </motion.h1>
 
                 <motion.p
