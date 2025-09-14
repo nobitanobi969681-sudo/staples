514bc32ac14da5109f5eaf97f1661e57f57c8859
Himanshu Kashyap <hkashyap.developer@gmail.com>
2025-09-14 21:16:24 +0530

    Stable

--- components/custom/Prefooter/Prefooter.tsx
+++ components/custom/Prefooter/Prefooter.tsx
@@ -5,14 +5,10 @@ import { SparklesText } from "@/components/magicui/sparkles-text";
 export default function CoverDemo({ align }: any) {
   return (
     <div>
-      <h1 className="max-w-7xl mx-auto relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
-        <SparklesText
-          sparklesCount={0}
-          className="text-center xl:text-left font-medium text-black dark:text-white text-[34px] sm:text-5xl 2xl:text-6xl px-2 sm:px-0"
-        >
-          <span className="leading-relaxed">{align} Media Solutions @</span>{" "}
-          <Cover>warp speed </Cover>
-        </SparklesText>
+      <h1 className="max-w-2xl mx-auto relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
+        <span className="text-center xl:text-left font-bold leading-tight text-white text-[34px] sm:text-6xl 2xl:text-7xl px-2 sm:px-0">
+          Digital Experiences That Inspire
+        </span>
       </h1>
     </div>
   );
--- components/custom/herobanner/herobanner.tsx
+++ components/custom/herobanner/herobanner.tsx
@@ -6,8 +6,8 @@ import Link from "next/link";
 
 export default function UniqueHeroWithSidebar() {
   return (
-    <div className=" dark:bg-black mx-auto relative flex justify-center flex-col md:flex-row bg-[url('/images/herobg.jpg')] bg-top bg-cover border-b">
-      <div className="bg-[rgba(255,255,255,0.92)] dark:bg-[rgba(0,0,0,0.8)] pb-12 sm:pb-8 xl:pb-0 w-full min-h-[74vh] flex">
+    <div className=" dark:bg-black mx-auto relative flex justify-center flex-col md:flex-row bg-[url('/images/herobg.jpg')] bg-center bg-cover border-b">
+      <div className="bg-[rgba(0,0,0,0.34)] dark:bg-[rgba(0,0,0,0.8)] pb-12 sm:pb-8 xl:pb-0 w-full min-h-[74vh] flex">
         {/* Hero Section */}
         <main className="flex-1 flex items-center justify-center px-1 sm:px-6 py-8 pb-2 sm:py-16">
           <div className=" grid grid-cols-1 xl:grid-cols-2 gap-10 items-center">
@@ -16,9 +16,9 @@ export default function UniqueHeroWithSidebar() {
             <div className="flex justify-center xl:justify-end pl-0 xl:pl-20">
               <div className="max-w-[800px] space-y-6 text-center xl:text-left">
                 <Prefooter />
-                <p className="mx-auto xl:mx-0 max-w-[560px] text-xl text-muted-foreground mb-8 px-2">
-                  We craft scalable, responsive, and user-friendly platforms to
-                  grow your digital presence.
+                <p className="mx-auto xl:mx-0 max-w-[560px] text-xl  mb-8 px-2 text-white">
+                  We craft premium digital solutions, blending creativity and
+                  technology to elevate your brand’s online presence.
                 </p>
 
                 <div className="flex flex-col sm:flex-row items-center gap-3 justify-center xl:justify-start">
@@ -32,7 +32,7 @@ export default function UniqueHeroWithSidebar() {
             </div>
 
             {/* Visual / Image Block */}
-            <Card className="shadow-lg overflow-hidden rounded-4xl 2xl:ml-20 max-w-[680px] mx-4 sm:mx-auto ">
+            <Card className=" overflow-hidden rounded-4xl 2xl:ml-20 max-w-[680px] mx-4 sm:mx-auto ">
               <CardContent className="p-0">
                 <img
                   src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?q=80&w=1200&auto=format&fit=crop"
