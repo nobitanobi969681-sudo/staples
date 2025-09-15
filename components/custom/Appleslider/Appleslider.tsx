705d8eaabbc78bb07646873479a6cec4497a99ae
Himanshu Kashyap <hkashyap.developer@gmail.com>
2025-09-15 13:57:11 +0530

    Stable

--- components/custom/Appleslider/Appleslider.tsx
+++ components/custom/Appleslider/Appleslider.tsx
@@ -2,7 +2,9 @@
 
 import React from "react";
 import { Carousel, Card } from "@/components/ui/apple-cards-carousel";
-
+import { ShimmerButton } from "@/components/magicui/shimmer-button";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 export default function AppleCardsCarouselDemo() {
   const cards = data.map((card, index) => (
     <Card key={card.src} card={card} index={index} />
@@ -18,30 +20,78 @@ export default function AppleCardsCarouselDemo() {
   );
 }
 
-const DummyContent = () => {
+const DummyContentOne = () => {
   return (
     <>
-      {[...new Array(3).fill(1)].map((_, index) => {
+      {[...new Array(1).fill(1)].map((_, index) => {
         return (
           <div
-            key={"dummy-content" + index}
-            className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4"
+            key={"dummy-content-one" + index}
+            className="bg-[#F5F5F7] dark:bg-neutral-800 p-4 md:p-14 rounded-3xl mb-4"
           >
-            <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans max-w-3xl mx-auto">
+            <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans  mb-8">
+              We design and develop{" "}
               <span className="font-bold text-neutral-700 dark:text-neutral-200">
-                The first rule of Apple club is that you boast about Apple club.
+                high-performance websites and apps
               </span>{" "}
-              Keep a journal, quickly jot down a grocery list, and take amazing
-              class notes. Want to convert those notes to text? No problem.
-              Langotiya jeetu ka mara hua yaar is ready to capture every
-              thought.
+              tailored to your business goals. Our approach blends clean code,
+              intuitive UI/UX, and scalable architecture, ensuring seamless
+              functionality across devices. From concept to launch, we deliver
+              reliable digital solutions that engage users and drive measurable
+              growth.
             </p>
+            <Link href="/new-quote">
+              <Button variant="outline" className="text-xl cursor-pointer">
+                Get A Quote
+              </Button>
+            </Link>
+
             <img
               src="https://assets.aceternity.com/macbook.png"
               alt="Macbook mockup from Aceternity UI"
               height="500"
               width="500"
-              className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain"
+              className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain hidden"
+            />
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+const DummyContentTwo = () => {
+  return (
+    <>
+      {[...new Array(1).fill(1)].map((_, index) => {
+        return (
+          <div
+            key={"dummy-content-two" + index}
+            className="bg-[#F5F5F7] dark:bg-neutral-800 p-4 md:p-14 rounded-3xl mb-4"
+          >
+            <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans  mb-8">
+              We craft{" "}
+              <span className="font-bold text-neutral-700 dark:text-neutral-200">
+                intuitive interfaces and stunning visuals
+              </span>{" "}
+              that blend creativity with usability. Our UI/UX process
+              prioritizes seamless user journeys, while our graphic design
+              elevates brand identity with precision. From wireframes to
+              polished assets, we deliver cohesive, high-impact designs that
+              engage audiences and strengthen your brand presence.
+            </p>
+            <Link href="/new-quote">
+              <Button variant="outline" className="text-xl cursor-pointer">
+                Get A Quote
+              </Button>
+            </Link>
+
+            <img
+              src="https://assets.aceternity.com/macbook.png"
+              alt="Macbook mockup from Aceternity UI"
+              height="500"
+              width="500"
+              className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain hidden"
             />
           </div>
         );
@@ -52,40 +102,40 @@ const DummyContent = () => {
 
 const data = [
   {
-    category: "Get a stunning website",
+    category: "Digitize your business",
     title: "Website & App Development",
     src: "/services/website-development.jpg",
-    content: <DummyContent />,
+    content: <DummyContentOne />,
   },
   {
     category: "Product",
     title: "Digital Marketing & SEO",
     src: "/services/digital-marketing.jpg",
-    content: <DummyContent />,
+    content: <DummyContentTwo />,
   },
   {
     category: "Product",
     title: "Photograpy & Video Editing",
     src: "/services/graphic-designing.jpg",
-    content: <DummyContent />,
+    content: <DummyContentOne />,
   },
   {
     category: "R&D",
     title: "Building MPVs & Prototypes",
     src: "/services/mvp.jpg",
-    content: <DummyContent />,
+    content: <DummyContentOne />,
   },
   {
     category: "Productivity",
     title: "UI/UX & Graphic Designing",
     src: "/services/uiux.jpg",
-    content: <DummyContent />,
+    content: <DummyContentOne />,
   },
 
   {
     category: "Hiring",
     title: "Branding & PR Management",
     src: "/services/pr.jpg",
-    content: <DummyContent />,
+    content: <DummyContentOne />,
   },
 ];
