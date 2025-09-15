4d64b770f1172b1e7bab2ab0e12df4096a223b10
Himanshu Kashyap <hkashyap.developer@gmail.com>
2025-09-15 16:21:37 +0530

    Stable

--- components/custom/Appleslider/Appleslider.tsx
+++ components/custom/Appleslider/Appleslider.tsx
@@ -100,6 +100,166 @@ const DummyContentTwo = () => {
   );
 };
 
+const DummyContentThree = () => {
+  return (
+    <>
+      {[...new Array(1).fill(1)].map((_, index) => {
+        return (
+          <div
+            key={"dummy-content-three" + index}
+            className="bg-[#F5F5F7] dark:bg-neutral-800 p-4 md:p-14 rounded-3xl mb-4"
+          >
+            <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans  mb-8">
+              We craft{" "}
+              <span className="font-bold text-neutral-700 dark:text-neutral-200">
+                intuitive interfaces and stunning visuals
+              </span>{" "}
+              that blend creativity with usability. Our UI/UX process
+              prioritizes seamless user journeys, while our graphic design
+              elevates brand identity with precision. From wireframes to
+              polished assets, we deliver cohesive, high-impact designs that
+              engage audiences and strengthen your brand presence.
+            </p>
+            <Link href="/new-quote">
+              <Button variant="outline" className="text-xl cursor-pointer">
+                Get A Quote
+              </Button>
+            </Link>
+
+            <img
+              src="https://assets.aceternity.com/macbook.png"
+              alt="Macbook mockup from Aceternity UI"
+              height="500"
+              width="500"
+              className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain hidden"
+            />
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+const DummyContentFour = () => {
+  return (
+    <>
+      {[...new Array(1).fill(1)].map((_, index) => {
+        return (
+          <div
+            key={"dummy-content-four" + index}
+            className="bg-[#F5F5F7] dark:bg-neutral-800 p-4 md:p-14 rounded-3xl mb-4"
+          >
+            <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans  mb-8">
+              We craft{" "}
+              <span className="font-bold text-neutral-700 dark:text-neutral-200">
+                intuitive interfaces and stunning visuals
+              </span>{" "}
+              that blend creativity with usability. Our UI/UX process
+              prioritizes seamless user journeys, while our graphic design
+              elevates brand identity with precision. From wireframes to
+              polished assets, we deliver cohesive, high-impact designs that
+              engage audiences and strengthen your brand presence.
+            </p>
+            <Link href="/new-quote">
+              <Button variant="outline" className="text-xl cursor-pointer">
+                Get A Quote
+              </Button>
+            </Link>
+
+            <img
+              src="https://assets.aceternity.com/macbook.png"
+              alt="Macbook mockup from Aceternity UI"
+              height="500"
+              width="500"
+              className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain hidden"
+            />
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+const DummyContentFive = () => {
+  return (
+    <>
+      {[...new Array(1).fill(1)].map((_, index) => {
+        return (
+          <div
+            key={"dummy-content-five" + index}
+            className="bg-[#F5F5F7] dark:bg-neutral-800 p-4 md:p-14 rounded-3xl mb-4"
+          >
+            <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans  mb-8">
+              We craft{" "}
+              <span className="font-bold text-neutral-700 dark:text-neutral-200">
+                intuitive interfaces and stunning visuals
+              </span>{" "}
+              that blend creativity with usability. Our UI/UX process
+              prioritizes seamless user journeys, while our graphic design
+              elevates brand identity with precision. From wireframes to
+              polished assets, we deliver cohesive, high-impact designs that
+              engage audiences and strengthen your brand presence.
+            </p>
+            <Link href="/new-quote">
+              <Button variant="outline" className="text-xl cursor-pointer">
+                Get A Quote
+              </Button>
+            </Link>
+
+            <img
+              src="https://assets.aceternity.com/macbook.png"
+              alt="Macbook mockup from Aceternity UI"
+              height="500"
+              width="500"
+              className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain hidden"
+            />
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+const DummyContentSix = () => {
+  return (
+    <>
+      {[...new Array(1).fill(1)].map((_, index) => {
+        return (
+          <div
+            key={"dummy-content-six" + index}
+            className="bg-[#F5F5F7] dark:bg-neutral-800 p-4 md:p-14 rounded-3xl mb-4"
+          >
+            <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-2xl font-sans  mb-8">
+              We craft{" "}
+              <span className="font-bold text-neutral-700 dark:text-neutral-200">
+                intuitive interfaces and stunning visuals
+              </span>{" "}
+              that blend creativity with usability. Our UI/UX process
+              prioritizes seamless user journeys, while our graphic design
+              elevates brand identity with precision. From wireframes to
+              polished assets, we deliver cohesive, high-impact designs that
+              engage audiences and strengthen your brand presence.
+            </p>
+            <Link href="/new-quote">
+              <Button variant="outline" className="text-xl cursor-pointer">
+                Get A Quote
+              </Button>
+            </Link>
+
+            <img
+              src="https://assets.aceternity.com/macbook.png"
+              alt="Macbook mockup from Aceternity UI"
+              height="500"
+              width="500"
+              className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain hidden"
+            />
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
 const data = [
   {
     category: "Digitize your business",
@@ -108,7 +268,7 @@ const data = [
     content: <DummyContentOne />,
   },
   {
-    category: "Product",
+    category: "Designs That Inspire Engagement",
     title: "Digital Marketing & SEO",
     src: "/services/digital-marketing.jpg",
     content: <DummyContentTwo />,
@@ -117,25 +277,25 @@ const data = [
     category: "Product",
     title: "Photograpy & Video Editing",
     src: "/services/graphic-designing.jpg",
-    content: <DummyContentOne />,
+    content: <DummyContentThree />,
   },
   {
     category: "R&D",
     title: "Building MPVs & Prototypes",
     src: "/services/mvp.jpg",
-    content: <DummyContentOne />,
+    content: <DummyContentFour />,
   },
   {
-    category: "Productivity",
+    category: "Designs That Inspire Engagement",
     title: "UI/UX & Graphic Designing",
     src: "/services/uiux.jpg",
-    content: <DummyContentOne />,
+    content: <DummyContentFive />,
   },
 
   {
     category: "Hiring",
     title: "Branding & PR Management",
     src: "/services/pr.jpg",
-    content: <DummyContentOne />,
+    content: <DummyContentSix />,
   },
 ];
