7439a393587bda3b24dd92fb2aa7f0055ccfe68d
Himanshu Kashyap <hkashyap.developer@gmail.com>
2025-09-16 05:03:24 +0530

    Stable

--- app/about/herobanner.tsx
+++ app/about/herobanner.tsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 export default function AboutPage() {
   return (
     <div className=" dark:bg-black mx-auto relative  md:flex-row bg-[url('/images/herobg.jpg')] bg-bottom bg-cover border-b">
-      <div className="bg-[rgba(255,255,255,0)] dark:bg-[rgba(0,0,0,0.8)] min-h-[320px] sm:min-h-[400px]   xl:pb-0 w-full flex justify-center flex-col">
+      <div className="bg-[rgba(255,255,255,0)] dark:bg-[rgba(0,0,0,0.8)] min-h-[280px] sm:min-h-[400px]   xl:pb-0 w-full flex justify-center flex-col">
         <div className="relative w-full max-w-4xl text-center mx-auto">
           <motion.h1
             initial={{ opacity: 0, y: -30 }}
