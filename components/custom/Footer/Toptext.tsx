f385581956b262a41a46254fd101b4a8c5cd2b02
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2025-09-19 18:25:58 +0530

    Stable

--- components/custom/Footer/Toptext.tsx
+++ components/custom/Footer/Toptext.tsx
@@ -5,7 +5,7 @@ const Toptext = () => {
   return (
     <div className="mb-12 sm:mb-12 md:mb-28 text-5xl max-w-[880px]">
       <p className="font-bold tracking-tighter text-3xl lg:text-4xl text-black dark:text-white text-center md:text-left">
-        <AuroraText>© MXI — Let's go Red.</AuroraText>
+        © MXI — Let's go <span className="text-red-600">Red</span>.
       </p>
     </div>
   );
