540c1954b7ac128b5162fa2319b30ec5a73161c3
Himanshu Kashyap <87484630+hkashyap-developer@users.noreply.github.com>
2025-09-19 21:23:22 +0530

    Stable

--- components/custom/herobanner/herobanner.tsx
+++ components/custom/herobanner/herobanner.tsx
@@ -12,8 +12,8 @@ export default function UniqueHeroWithSidebar() {
     <div className="relative bg-black overflow-hidden border-b-[1px] border-gray-600">
       <Grid size={20} />
       <div className="  mx-auto relative flex justify-center flex-col md:flex-row  bg-center bg-cover bg-[rgba(0,0,0,0.6)] pt-0">
-        <div className="block sm:hidden sm:px-4 mb-10 sm:mb-0">
-          <div className="bg-[url('/mob-home-hero.png')] bg-cover bg-center h-80 bg-cover w-full rounded-0"></div>
+        <div className="block sm:hidden sm:px-4 mb-4 sm:mb-0">
+          <div className="bg-[url('/mob-home-hero.png')] bg-cover bg-center h-60 bg-cover w-full rounded-0"></div>
         </div>
         <div className="pb-16 sm:pb-8 xl:pb-0 w-full flex">
           {/* Hero Section */}
